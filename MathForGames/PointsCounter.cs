5bbb3082c1ed16804453b2a9bab862bb402c571c
Brittnie <brittniesexton95@gmail.com>
2021-10-27 15:07:49 -0500

    Started created a point counter

---
+++ MathForGames/PointsCounter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Raylib_cs;
+
+namespace MathForGames
+{
+    class PointsCounter : UIText
+    {
+        public Bullets _bullets;
+        public Player _player;
+
+        public PointsCounter(float x, float y, Bullets bullets, Color color, string name)
+            : base(x, y, color, name)
+        {
+            _bullets = bullets;
+            Text = "Points: " + 
+        }
+    }
+}
